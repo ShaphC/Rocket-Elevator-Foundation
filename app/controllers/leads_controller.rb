aceac9355610551760976f3a2439c5652c3f416f
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-08 14:03:00 -0500

    figaro and rake tasks are back

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -25,7 +25,7 @@ class LeadsController < ApplicationController
 
     private
     def fact_contacts
-      dwh = PG::Connection.new(host: 'codeboxx-postgresql.cq6zrczewpu2.us-east-1.rds.amazonaws.com', port: 5432, dbname: "MaximeAuger_psql", user: "codeboxx", password: "Codeboxx1!")
+      dwh = PG::Connection.new(port: 5432, dbname: "MaximeAuger_psql", user: "postgres", password: "postgres")
       dwh.exec("TRUNCATE fact_contacts")
 
       dwh.prepare('to_fact_contacts', 'INSERT INTO fact_contacts (contact_id, creation_date, company_name, email, project_name, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)')
@@ -38,3 +38,5 @@ class LeadsController < ApplicationController
       :message, :file_attachment, :file, :image)
     end
 end
+
+# connection = ActiveRecord::Base.establish_connection(:dwh_development)
