78c667854ef890b21d99a43add51fba2e71cac1a
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-08 17:32:11 -0500

    watson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 require 'pg'
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    protect_from_forgery with: :exception
-    
-    
-
-    
+    protect_from_forgery with: :exception   
 end 
 
---
+++ app/controllers/watson_controller.rb
@@ -0,0 +1,2 @@
+class WatsonController < ApplicationController
+end
