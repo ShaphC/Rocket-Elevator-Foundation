7ce2f31f6dd8cdd44273e64460f6b31d2577f3d8
ShaphanCharles <web_shaph@hotmail.com>
2021-03-09 15:29:15 -0500

    Twilio App ready

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,19 +1,25 @@
 class Elevator < ApplicationRecord
-    after_update :call, 
-        :if => Proc.new {|elevators| elevators.status == 'Intervention'} 
+    after_update :elevator_status_change, :send_message, 
+        :if => proc {|elevator| elevator.status == 'Intervention'}
     belongs_to :column, optional: true
 
-    # @elevator = Elevator.find(515)
-    # Elevator.all.each do |elevators|
-    #     puts elevators.status
-    # end
-    
-    def call
+    def elevator_status_change
+        @elevator = Elevator.order(:updated_at).last
+    end
+
+    def initialize(message)
+        @message = message
+    end
+
+    #@elevator = Elevator.
+
+    def send_message
+    message = "The elevator ID is...#{@elevator.id} and the Serial number is...#{@elevator.serial_number}"
     client = Twilio::REST::Client.new
         client.messages.create({
             from: ENV["twilio_phone_number"],
             to: ENV["twilio_user_number"],
-            body: "The elevator ID is coming."
+            body: message
         })
     end
 end
