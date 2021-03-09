1fc650e79ac905f2bbd846856fc544e8efee614a
danigrum <danigrum@gmail.com>
2021-03-09 10:02:30 -0500

    initializer: create 1 for zendesk, controller: delete the zendesk one

--- app/helpers/zendesk_helper.rb
+++
@@ -1,2 +0,0 @@
-module ZendeskHelper
-end
--- test/controllers/zendesk_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ZendeskControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
