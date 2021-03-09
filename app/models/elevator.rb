57dadb1cdd90f2517e680faf8bca080db42a65cc
ShaphanCharles <web_shaph@hotmail.com>
2021-03-09 13:39:31 -0500

    Twilio Condition added

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,3 +1,20 @@
 class Elevator < ApplicationRecord
-    belongs_to :column ,optional: true
+    after_update :call, 
+        :if => Proc.new {|elevators| elevators.status == 'Intervention'} 
+    belongs_to :column, optional: true
+
+    # @elevator = Elevator.find(515)
+    # Elevator.all.each do |elevators|
+    #     puts elevators.status
+    # end
+    
+    def call
+    client = Twilio::REST::Client.new
+        client.messages.create({
+            from: ENV["twilio_phone_number"],
+            to: ENV["twilio_user_number"],
+            body: "The elevator ID is coming."
+        })
+    end
 end
+
