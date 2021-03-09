de4238da32d23979dc6f51e155075c976d8c6d0a
adriengobeil3 <77251131+adriengobeil3@users.noreply.github.com>
2021-03-09 18:58:48 -0500

    Update elevator.rb

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -7,20 +7,12 @@ class Elevator < ApplicationRecord
         @elevator = Elevator.order(:updated_at).last
     end
 
-    def initialize(message)
-        @message = message
-    end
-
-    #@elevator = Elevator.
-
     def send_message
-    message = "The elevator ID is...#{@elevator.id} and the Serial number is...#{@elevator.serial_number}"
     client = Twilio::REST::Client.new
         client.messages.create({
             from: ENV["twilio_phone_number"],
             to: ENV["twilio_user_number"],
-            body: message
+            body: "The elevator ID is...#{@elevator.id} and the Serial number is...#{@elevator.serial_number}"
         })
     end
 end
-
