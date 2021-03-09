b2f93162e7e1c23d599be8770e2b3b13421e3a1d
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-09 10:25:04 -0500

    watson controller

--- app/controllers/watson_controller.rb
+++ app/controllers/watson_controller.rb
@@ -1,2 +1,28 @@
+require "ibm_watson"
+include IBMWatson
+
 class WatsonController < ApplicationController
+    def textToSpeech
+        authenticator = ENV["WATSON_TTS_API_KEY"]
+        
+        textToSpeech = IBMWatson::TextToSpeechV1.new(
+        authenticator: authenticator
+        )
+
+        textToSpeech.service_url = ENV["WATSON_TTS_URL"]
+
+        message = "Hello, this is a test message!"
+        
+        response = textToSpeech.synthesize(
+            text: message,
+            accept: "audio/mp3",
+            voice: "en-US_AllisonVoice"
+        ).result
+        
+        File.open("#{Rails.root}/public/outputs.mp3", "wb") do |audio_file|
+            audio_file.write(response)
+        end
+    end
 end
+
+#ActionController??
