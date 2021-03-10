dcff42652f5933e3ec35065c84512348594a5003
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-10 14:53:06 -0500

    chnged slack link

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,3 +1,3 @@
 module SlackNotifier
-    CLIENT = Slack::Notifier.new ENV["slack_token"]
+    CLIENT = Slack::Notifier.new "https://hooks.slack.com/services/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"
 end
