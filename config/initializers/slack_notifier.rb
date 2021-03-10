a1505a3af71dcacc413ca1375dbd9177131b2d00
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-10 15:49:45 -0500

    remove hard coded api key

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,3 +1,3 @@
 module SlackNotifier
-    CLIENT = Slack::Notifier.new "https://hooks.slack.com/services/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"
+    CLIENT = Slack::Notifier.new ENV["slack_token"]
 end
