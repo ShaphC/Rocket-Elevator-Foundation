9130db96458ed950b01127bc61a7cb5db273afc1
adriengobeil3 <adirengobeil3@gmail.com>
2021-03-11 14:34:52 -0500

    auth token

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -7,6 +7,8 @@ require 'zendesk_api'
 
 
 class LeadsController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
     
     def new
         @lead = Lead.new
@@ -95,6 +97,7 @@ class LeadsController < ApplicationController
         dwh.exec_prepared('to_fact_contacts', [ldcontact.id, ldcontact.created_at, ldcontact.company_name, ldcontact.email, ldcontact.project_name])
         end
     end
+
     def lead_params
         params.require(:lead).permit(:full_name, :email, :phone, :company_name, :project_name, :department, :project_description,
         :message, :file_attachment, :file, :image)
